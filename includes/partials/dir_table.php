1c0c9c963ed1632ecfd311fecaca11b84849047a
devjacjef <dev.jac.jef@proton.me>
2025-08-25 07:05:21 +0100

    Made the `dir_table` more composable.
    The dir table is now more composable and can determine the path its
    using dynamically as well as check if it's a file and just open the file
    contents instead.

--- includes/partials/dir_table.php
+++ includes/partials/dir_table.php
@@ -1,21 +1,64 @@
 <?php
 // NOTE: SITE_PATH is defined in header.php.
-// TODO: Later update this to only show demos.
-if(isset($path) && !empty($path)) {
-    $fsi = new FilesystemIterator($path);
-} else {
-    $fsi = new FilesystemIterator(SITE_PATH);
-}
-foreach ($fsi as $f): ?>
+// TODO: Tidy this file up a bit more later.
 
+// Checking if path is not set or is empty.
+// Defaults to SITE_PATH or gets the path.
+if (!isset($_GET['path']) || empty($_GET['path'])) {
+    $path = SITE_PATH;
+} else {
+    $path = $_GET['path'];
+}
+
+
+// Exit script early if the path cannot be determined.
+if (!file_exists($path)) {
+    echo 'Requested resource was not found.';
+    return;
+}
+
+// If directory, iterate all files in file system
+// else if file, get contents and display them.
+if (is_dir($path)) {
+    $fsi = new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS);
+} else {
+    echo file_get_contents($path);
+    return;
+}
+
+$hasFiles = false;
+foreach ($fsi as $file) {
+    $hasFiles = true;
+    break;
+}
+?>
+
+<table id="dirListTable">
     <tr>
-        <td>
-            <a href="<?= urlencode($f->getFilename()) ?>">
-                <?= htmlspecialchars($f->getFilename()) ?>
-            </a>
-        </td>
-        <td><?= byteConvert($f->getSize()) ?></td>
-        <td><?= date('d F y', filemtime($f)) ?></td>
+        <th>Name</th>
+        <th>Size</th>
+        <th>Last Opened</th>
     </tr>
 
-<?php endforeach; ?>
+    <?php if (!$hasFiles): ?>
+        <tr>
+            <td colspan="3">
+                Nothing to show here.
+            </td>
+        </tr>
+    <?php else: ?>
+
+        <?php foreach ($fsi as $f): ?>
+            <tr>
+                <td>
+                    <a href="?path=<?= urlencode($f->getFilename()) ?>">
+                        <?= htmlspecialchars($f->getFilename()) ?>
+                    </a>
+                </td>
+                <td><?= byteConvert($f->getSize()) ?></td>
+                <td><?= date('d F y', filemtime($f)) ?></td>
+            </tr>
+        <?php endforeach; ?>
+
+    <?php endif; ?>
+</table>
--- index.php
+++ index.php
@@ -5,14 +5,7 @@ error_reporting(E_ALL);
 ?>
 
 <?php include __DIR__ . '/includes/header.php' ?>
-<table id="dirListTable">
-    <tr>
-        <th>Name</th>
-        <th>Size</th>
-        <th>Last Opened</th>
-    </tr>
 
-    <?php include __DIR__ . '/includes/partials/dir_table.php' ?>
-</table>
+<?php include __DIR__ . '/includes/partials/dir_table.php' ?>
 
 <?php include __DIR__ . '/includes/footer.php' ?>
