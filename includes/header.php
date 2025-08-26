e8870c46ea27b9986317d31ebf9b5835e26e9afd
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-26 13:05:14 +0100

    Tidied header source file and added styling
    Header now has a class for styling.

--- includes/header.php
+++ includes/header.php
@@ -9,12 +9,17 @@ include SITE_PATH . '/src/php/helpers.php';
 
 <head>
     <meta name="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="src/css/style.css">
+    <noscript>
+        <!--This is a little joke.-->
+        By disabling JS, you've made the right choice. Enjoy your freedom.
+    </noscript>
 </head>
 
-<body>
 
-<noscript>
-<!--This is a little joke.-->
-By disabling JS, you've made the right choice. Enjoy your freedom.
-</noscript>
+<body>
+<div class="wrapper">
+    <header class="page-header">
+        <a href="?path=/" class="logo">layground</a>
+    </header>
