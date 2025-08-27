bee1d8730fd9d7012644933673c504e0e47bfe86
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-27 16:11:25 +0100

    Plans to create per-row deletion.

--- includes/partials/file_toolbar.php
+++ includes/partials/file_toolbar.php
@@ -1,6 +1,7 @@
 <div id="fileBrowserWrapper">
     <form id="fileToolbar" method="POST">
         <button name="create" type="submit">Create</button>
+        <button name="delete" type="delete">Delete</button>
         <button name="upload" type="submit">Upload</button>
         <button name="download" type="submit">Download</button>
     </form>
