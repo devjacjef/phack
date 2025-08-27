b9940767ebcb2e2ce4ec65104b41540a47959756
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-27 17:32:18 +0100

    Adjusting structure and styling for file browser
    Made some adjustments to the structure and styling of file browser.
    Looking to follow a 'better structure'.

--- includes/partials/file_toolbar.php
+++ includes/partials/file_toolbar.php
@@ -1,8 +1,16 @@
 <div id="fileToolbarWrapper">
-    <form id="fileToolbar" method="POST">
-        <button name="create" type="submit">New</button>
-        <button name="delete" type="delete" hidden>Delete</button>
-        <button name="upload" type="submit">Add Files</button>
-        <button name="download" type="submit" hidden>Download</button>
-    </form>
+     <div id="fileToolbarActions">
+        <button>Add Files</button>
+        <button>New</button>
+     </div>
+     <div id="fileToolbarRowActions">
+        <button>Download</button>
+        <button>View</button>
+        <button>Copy</button>
+        <button>Move</button>
+        <button>Rename</button>
+        <button>Zip</button>
+        <button>Delete</button>
+        <button>Copy Link</button>
+     </div>
 </div>
