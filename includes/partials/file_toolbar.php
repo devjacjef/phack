939fa5a24688b1cc45310aa65df362fe1c3be877
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-27 17:13:57 +0100

    Minor adjustment for the toolbar.

--- includes/partials/file_toolbar.php
+++ includes/partials/file_toolbar.php
@@ -1,8 +1,8 @@
 <div id="fileToolbarWrapper">
     <form id="fileToolbar" method="POST">
-        <button name="create" type="submit">Create</button>
-        <button name="delete" type="delete">Delete</button>
-        <button name="upload" type="submit">Upload</button>
-        <button name="download" type="submit">Download</button>
+        <button name="create" type="submit">New</button>
+        <button name="delete" type="delete" hidden>Delete</button>
+        <button name="upload" type="submit">Add Files</button>
+        <button name="download" type="submit" hidden>Download</button>
     </form>
 </div>
