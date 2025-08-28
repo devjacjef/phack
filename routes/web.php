7ba0777378d19273d8c126c69e0e520dd98985a9
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-28 09:25:05 +0100

    Rough 'sketch' of directory structure.
    All functionality should be broken and needs a focused session to fix it
    all.
    This structure going forward should provide an 'easier' workflow going
    forward. With clear and consise constraints on what should go where
    ensuring a smooth workflow.

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Routes for this project.
+ *
+ * uri - Resources (/path/to/resource)
+ * handler - Class that handles request [CLASS, FUNCTION]
+ * method - Method of request [GET, POST, PUT, PATCH, DELETE...]
+ * */
+
+// TODO: Refactor later...
+return [
+    ['uri' => '', 'handler' => ['', ''], 'method'],
+];
+?>
