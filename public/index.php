75ab06251850e1d4ed4afba8c4843fb5b483a7a9
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-28 13:25:54 +0100

    Implemented a demo into the site.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,24 @@
 <?php
+declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-?>
 
-<?php include __DIR__ . '/includes/header.php' ?>
+// Loads defined constants and other configs.
+define('SITE_PATH', dirname(__DIR__));
+require_once SITE_PATH . '/config/app.php';
 
-<div class="main">
-<?php include __DIR__ . '/includes/partials/bread_crumbs.php' ?>
-<?php include __DIR__ . '/includes/partials/file_browser.php' ?>
-</div>
+require_once SITE_PATH . '/core/helpers.php';
+require_once SITE_PATH . '/core/router.php';
 
-<?php include __DIR__ . '/includes/footer.php' ?>
+$router = new Router();
+
+$route = new Route('GET', '/', function() {
+    echo 'hi';
+});
+
+$router->addRoute($route);
+
+$request = new Request();
+
+$router->dispatch($request);
